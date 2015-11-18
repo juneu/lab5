7fd85e734166e814b979bd69936936c41fae7e9b
Julia Neugebauer <juneug@pc02.pool.tp1.uni-duesseldorf.de>
2015-11-18 18:06:08 +0100

    lab5

--- randomwalk.cxx
+++ randomwalk.cxx
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <sstream>
 #include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
@@ -11,6 +12,9 @@ struct colloid{
 
 void init(colloid* const c, const int N);
 void print(const colloid* const c, const int N, const string fname);
+void random(const int N, int* rx, int* ry);
+void pusher(colloid* const c, const int N, const int* rx, const int* ry);
+void statistics(colloid* const c, const int N, double& meanx, double& meany, double& var);
 
 int main(void){
   
@@ -42,11 +46,11 @@ int main(void){
     
     srand(time(NULL));			// randomize random numbers
     
-    for(int i = 1; i <= Nfiles; i++){
-	for(int j = 0; j < Nsubsteps; j++){
-	    // call to function which randomly sets up rx and ry
-	    // call to function which pushes all colloids according to rx and ry
-	    // call to function which evaluates statistics
+    for(int i = 1; i <= Nfiles; i++){ // um die einzelnen files aufzurufen (für 10 Schritte jeweils eine neue file)
+	for(int j = 0; j < Nsubsteps; j++){ //für jeden einzelnen Schritt einmal durch die for Schleife
+	    random(N, rx, ry); // call to function which randomly sets up rx and ry
+	    pusher(c, N, rx, ry);// call to function which pushes all colloids according to rx and ry
+	    statistics(c, N, meanx, meany, var);// call to function which evaluates statistics
 	    stat << (i-1)*Nsubsteps+j << "\t" << meanx << "\t";
 	    stat << meany << "\t" << var << endl;
 	}
@@ -60,7 +64,7 @@ int main(void){
     return 0;
 }
 
-void init(colloid* const c, const int N){
+void init(colloid* const c, const int N){ // init lässt alle Kolloide vom Ursprung starten
     for(int i = 0; i < N; i++){
 	c[i].x = 0;
 	c[i].y = 0;
@@ -72,4 +76,35 @@ void print(const colloid* const c, const int N, const string fname){
     for(int i = 0; i < N; i++)
 	out << c[i].x << "\t" << c[i].y << endl;
     out.close();
+}
+
+void random(const int N, int* rx, int* ry){
+ for(int i=0; i<N; i++){
+  rx[i]=int(3*(double(rand())/RAND_MAX))-1; //gleiche Wahrscheinlichkeit für -1 0 1
+  ry[i]=int(3*(double(rand())/RAND_MAX))-1;
+   }
+}
+
+void pusher(colloid* const c, const int N, const int* rx, const int* ry){
+  for(int i=0; i<N; i++){
+   c[i].x=c[i].x+rx[i]; // oder c[i].x +=rx[i]
+   c[i].y=c[i].y+ry[i];
+  }
+}
+
+void statistics(colloid* const c, const int N, double& meanx, double& meany, double& var){
+meanx=0;
+meany=0;
+var=0;
+for(int i=0; i<N; i++){
+meanx=meanx+c[i].x;
+meany=meany+c[i].y;
+};
+meanx=meanx/N; // weil N ein int ist, darf man hier nicht (1/N)*mean rechnen und wir können aus N keinen double Wert machen, weil die Länge des Arrays immer ein Integer sein muss
+meany=meany/N;
+
+for(int i=0; i<N; i++){
+   var=var+pow((c[i].x-meanx),2)+pow(c[i].y-meany,2);
+}
+var=var/N;
 }
